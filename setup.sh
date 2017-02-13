f1da2a4711fd4479abafecd6cf43d5382d11057c
Kevin Ho <keho98@users.noreply.github.com>
2017-02-12 16:27:34 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -162,21 +162,6 @@ while true; do
   esac
 done
 
-# A bunch of pre setup including making sure git is installed
-# sudo apt-get update
-# sudo apt-get upgrade -y
-# sudo apt-get install --no-install-recommends git zsh curl wget vim imagemagick silversearcher-ag -y
-
-# sudo apt-get install python3 python-pip -y
-# sudo pip install virtualenv
-
-# sudo apt-get install python3-setuptools -y
-# sudo easy_install3 pip
-# sudo pip3 install virtualenv
-
-# the following are required for some python libraries
-# sudo apt-get install libsqlite3-dev libbz2-dev tk8.6-dev
-
 install_zsh
 
 # Reload zsh settings
